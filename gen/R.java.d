7681914500ec2d584286b9fbd75288853fc8ea0c
harri feng <harri.feng@gmail.com>
2014-12-27 22:58:38 +0800

    the true or false example works!

--- gen/R.java.d
+++ gen/R.java.d
@@ -4,5 +4,6 @@ c:\Code\tmp\GeoQuiz\res\drawable-ldpi\ic_launcher.png \
 c:\Code\tmp\GeoQuiz\res\drawable-mdpi\ic_launcher.png \
 c:\Code\tmp\GeoQuiz\res\drawable-xhdpi\ic_launcher.png \
 c:\Code\tmp\GeoQuiz\res\layout\main.xml \
+c:\Code\tmp\GeoQuiz\res\values\ids.xml \
 c:\Code\tmp\GeoQuiz\res\values\strings.xml \
 c:\Code\tmp\GeoQuiz\bin\AndroidManifest.xml \
--- src/com/hfeng/geoquiz/MainActivity.java
+++ src/com/hfeng/geoquiz/MainActivity.java
@@ -2,14 +2,38 @@ package com.hfeng.geoquiz;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends Activity
 {
+    private Button mTrueButton;
+    private Button mFalseButton;
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+
+        mTrueButton = (Button)findViewById(R.id.true_button);
+        mTrueButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Toast.makeText(MainActivity.this,
+                                   R.string.incorrect_toast,
+                                   Toast.LENGTH_SHORT).show();
+                }
+            });
+        mFalseButton = (Button)findViewById(R.id.false_button);
+        mFalseButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Toast.makeText(MainActivity.this,
+                                   R.string.correct_toast,
+                                   Toast.LENGTH_SHORT).show();
+                }
+            });
     }
 }
