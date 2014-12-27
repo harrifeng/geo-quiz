f4de1a942c439752df1d3d82d68199c38b82b90c
harri feng <harri.feng@gmail.com>
2014-12-27 12:04:53 +0800

    slite changes

---
+++ gen/R.java.d
@@ -0,0 +1,8 @@
+c:\Code\tmp\GeoQuiz\gen\com\hfeng\geoquiz\R.java \
+ : c:\Code\tmp\GeoQuiz\res\drawable-hdpi\ic_launcher.png \
+c:\Code\tmp\GeoQuiz\res\drawable-ldpi\ic_launcher.png \
+c:\Code\tmp\GeoQuiz\res\drawable-mdpi\ic_launcher.png \
+c:\Code\tmp\GeoQuiz\res\drawable-xhdpi\ic_launcher.png \
+c:\Code\tmp\GeoQuiz\res\layout\main.xml \
+c:\Code\tmp\GeoQuiz\res\values\strings.xml \
+c:\Code\tmp\GeoQuiz\bin\AndroidManifest.xml \
