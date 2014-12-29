f261306288e0a46763e05d57e8b50d75f873165f
Harri Feng <harri.feng@gmail.com>
2014-12-28 19:04:50 -0800

    forget R.java.d it will change everytime the project change location

--- gen/R.java.d
+++
@@ -1,9 +0,0 @@
-c:\Code\tmp\GeoQuiz\gen\com\hfeng\geoquiz\R.java \
- : c:\Code\tmp\GeoQuiz\res\drawable-hdpi\ic_launcher.png \
-c:\Code\tmp\GeoQuiz\res\drawable-ldpi\ic_launcher.png \
-c:\Code\tmp\GeoQuiz\res\drawable-mdpi\ic_launcher.png \
-c:\Code\tmp\GeoQuiz\res\drawable-xhdpi\ic_launcher.png \
-c:\Code\tmp\GeoQuiz\res\layout\main.xml \
-c:\Code\tmp\GeoQuiz\res\values\ids.xml \
-c:\Code\tmp\GeoQuiz\res\values\strings.xml \
-c:\Code\tmp\GeoQuiz\bin\AndroidManifest.xml \
