e3ff313d603e339cc7032d41def2f4a500712e28
Harri Feng <harri.feng@gmail.com>
2014-12-28 23:17:59 -0800

    add a log, then find a bug

--- src/com/hfeng/geoquiz/MainActivity.java
+++ src/com/hfeng/geoquiz/MainActivity.java
@@ -2,6 +2,7 @@ package com.hfeng.geoquiz;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Button;
 import android.view.View;
 import android.widget.Toast;
@@ -9,6 +10,7 @@ import android.widget.TextView;
 
 public class MainActivity extends Activity
 {
+    private static final String TAG = "QuizActivity";
     private Button mTrueButton;
     private Button mFalseButton;
     private Button mNextButton;
@@ -47,6 +49,7 @@ public class MainActivity extends Activity
     public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
+        Log.d(TAG, "onCreate(Bundle) called");
         setContentView(R.layout.main);
 
         mQuestionTextView = (TextView)findViewById(R.id.question_text_view);
@@ -74,5 +77,30 @@ public class MainActivity extends Activity
                 }
             });
         updateQuestion();
+    }
+    @Override
+    public void onStart() {
+        super.onStart();
+        Log.d(TAG, "onStart() called");
+    }
+    @Override
+    public void onPause() {
+        super.onPause();
+        Log.d(TAG, "onPause() called");
+    }
+    @Override
+    public void onResume() {
+        super.onResume();
+        Log.d(TAG, "onResume() called");
+    }
+    @Override
+    public void onStop() {
+        super.onStop();
+        Log.d(TAG, "onStop() called");
+    }
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy() called");
     }
 }
